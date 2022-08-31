98f0091103b3b1bccb69c528e7343b211472c5e1
Claudio Martínez Ortiz <46463001+CMOSiemo@users.noreply.github.com>
2022-08-31 01:25:58 -0400

    v 1.02

--- my_car.rb
+++ my_car.rb
@@ -1,18 +1,16 @@
-#Copyright (c) 2022 Claudio Martínez Ortiz
+# frozen_string_literal: true
 
+# Copyright (c) 2022 Claudio Martínez Ortiz
+
+# Couldn't think of a cool, simple functionality to add with module
 module Towable
-  
-  # Couldn't think of a cool, simple functionality to add with module
   def can_tow?(pounds)
-
     pounds < 2000
-
   end
-
 end
 
+# Vehicle class
 class Vehicle
-
   # Getters and setters
   attr_accessor :color, :speed, :ignition
   attr_reader :make, :model, :year
@@ -20,34 +18,29 @@ class Vehicle
   # Instance counter
   @@vehicles = 0
 
-  def initialize(mk, md, y, c)
-
+  def initialize(make, model, year, color)
     # Instance attributes
-    @make = mk
-    @model = md
-    @year = y
-    self.color = c
+    @make = make
+    @model = model
+    @year = year
+    self.color = color
     self.speed = 0
     self.ignition = false
     @@vehicles += 1
-
   end
 
   def info(vehicle)
-
     # Returns general mostly immutable info on vehicle
     "\nYour #{vehicle} is a #{color} #{make} #{model} #{year}"
-
   end
 
   def status(vehicle)
-
     # Returns current engine and speed status of vehicle
-    if self.ignition == true && self.speed != 0
+    if ignition == true && speed != 0
 
       "\nYour #{vehicle} is going at #{speed}km/h"
 
-    elsif self.ignition == true
+    elsif ignition == true
 
       "\nYour #{vehicle} is running on standby"
 
@@ -56,288 +49,205 @@ class Vehicle
       "\nYour #{vehicle} is parked and the engine is off"
 
     end
-
   end
 
   def age(vehicle)
-
     # Returns age of vehicle
     "Your #{vehicle} is #{years} years old"
-
   end
 
   def start(vehicle)
-
     # Starts engine
-    if self.ignition == false
+    if ignition == false
 
       self.ignition = true
 
-    else puts start_error(vehicle)
+    else
+      puts start_error(vehicle)
 
     end
-
   end
 
   def stop(vehicle)
-
     # Stops engine
-    if self.ignition == true
+    if ignition == true
 
       self.ignition = false
 
-    else puts stop_error(vehicle)
+    else
+      puts stop_error(vehicle)
 
     end
-
   end
 
   def accelerate(kmhm, vehicle)
-
     # Pretty self explanatory
-    if self.ignition == true
+    if ignition == true
 
       self.speed += kmhm
 
-    else puts ignition_error(vehicle)
+    else
+      puts ignition_error(vehicle)
 
     end
-  
   end
 
   def brake(kmhm, vehicle)
-
     # Same as above
-    if self.speed > 0 && self.speed - kmhm > 0
+    if self.speed.positive? && (self.speed - kmhm).positive?
 
       self.speed -= kmhm
 
-    elsif self.speed > 0
+    elsif self.speed.positive?
 
       self.speed = 0
 
-    else vehicle_stopped_error(vehicle)
+    else
+      vehicle_stopped_error(vehicle)
 
     end
-  
   end
 
   def spray_paint(paint, vehicle)
-
     # Painting car
     self.color = paint
     puts "You have painted your #{vehicle} #{paint}"
-
   end
 
-  def self.mileage(liters, km, vehicle)
-
+  def self.mileage(liters, kilometers, vehicle)
     # Mileage class method, but in kilometers. Kilometerage
-    puts "#{km / liters} kilometers per liter of gas"
-
+    puts "#{kilometers / liters} kilometers per liter of gas"
   end
 
-  def to_s(vehicle = "vehicle")
-
+  def to_s(vehicle = 'vehicle')
     # Replace to_s method
     "My #{vehicle} is a #{color} #{make} #{model} #{year}"
-
   end
 
   private
 
   def self.ammount
-
     # Returns vehicles counter
     @@vehicles
-
   end
 
   def years
-
-  # Returns age of vehicle
-  Time.now.year - year
-
+    # Returns age of vehicle
+    Time.now.year - year
   end
 
   def start_error(vehicle)
-
     # Self explanatory
     puts "\n#{vehicle.capitalize} engine is already started"
-
   end
 
   def stop_error(vehicle)
-
     puts "\n#{vehicle.capitalize} engine is already off"
-
   end
 
   def ignition_error(vehicle)
-
     puts "\nCan't accelerate with the #{vehicle} engine off"
- 
   end
 
   def vehicle_stopped_error(vehicle)
-
     puts "\n#{vehicle.capitalize} is already stopped"
-
   end
-
 end
 
-
+# Most methods replace parent class methods but invoke them passing the TYPE
+#   constant of the subclass
 class MyCar < Vehicle
-
-  # Most methods replace parent class methods but invoke them passing the TYPE
-  # constant of the subclass
-  TYPE = "car"
-
-  def initialize(mk, md, y, c)
-
-    super(mk, md, y, c)
-
-  end
+  TYPE = 'car'
 
   def info
-
     super(TYPE)
-
   end
 
   def status
-
     super(TYPE)
-
   end
 
   def age
-
     super(TYPE)
-
   end
 
   def start
-
     super(TYPE)
-
   end
 
   def stop
-
     super(TYPE)
-
   end
 
   def accelerate(kmhm)
-
     super(kmhm, TYPE)
-  
   end
 
   def brake(kmhm)
-
     super(kmhm, TYPE)
-  
   end
 
   def spray_paint(paint)
-
     super(paint, TYPE)
-
   end
 
-  def self.mileage(liters, km)
-
-    super(liters, km, TYPE)
-
+  def self.mileage(liters, kilometers)
+    super(liters, kilometers, TYPE)
   end
 
   def to_s
-
     super(TYPE)
-
   end
-
 end
 
+# Same as MyCar but includes Towable module
 class MyTruck < Vehicle
-
-  # Same as MyCar but includes Towable module
   include Towable
-  TYPE = "truck"
-
-  def initialize(mk, md, y, c)
-
-    super(mk, md, y, c)
-
-  end
+  TYPE = 'truck'
 
   def info
-
     super(TYPE)
-
   end
 
   def status
-
     super(TYPE)
-
   end
 
   def age
-
     super(TYPE)
-
   end
 
   def start
-
     super(TYPE)
-
   end
 
   def stop
-
     super(TYPE)
-
   end
 
   def accelerate(kmhm)
-
     super(kmhm, TYPE)
-  
   end
 
   def brake(kmhm)
-
     super(kmhm, TYPE)
-  
   end
 
   def spray_paint(paint)
-
     super(paint, TYPE)
-
   end
 
-  def self.mileage(liters, km)
-
-    super(liters, km, TYPE)
-
+  def self.mileage(liters, kilometers)
+    super(liters, kilometers, TYPE)
   end
 
   def to_s
-
     super(TYPE)
-
   end
-
 end
 
 # Tests
-tocomocho = MyCar.new("Suzuki", "Baleno", 2019, "grey")
+tocomocho = MyCar.new('Suzuki', 'Baleno', 2019, 'grey')
 puts tocomocho.info
 puts tocomocho.status
 tocomocho.start
@@ -360,11 +270,11 @@ tocomocho.stop
 puts tocomocho.status
 tocomocho.accelerate(60)
 tocomocho.stop
-tocomocho.spray_paint("red")
+tocomocho.spray_paint('red')
 puts tocomocho.info
 MyCar.mileage(20, 300)
 puts tocomocho
-yunta = MyTruck.new("Mitsubishi", "L200", 2020, "white")
+yunta = MyTruck.new('Mitsubishi', 'L200', 2020, 'white')
 puts yunta.info
 puts yunta.status
 yunta.start
@@ -387,12 +297,12 @@ yunta.stop
 puts yunta.status
 yunta.accelerate(80)
 yunta.stop
-yunta.spray_paint("black")
+yunta.spray_paint('black')
 puts yunta.info
 MyTruck.mileage(30, 600)
 puts yunta
 puts yunta.can_tow?(1500)
-puts yunta.can_tow?(20000)
+puts yunta.can_tow?(20_000)
 puts Vehicle.ammount
 puts MyCar.ancestors
 puts MyTruck.ancestors
--- student_class.rb
+++ student_class.rb
@@ -1,33 +1,24 @@
-#Copyright (c) 2022 Claudio Martínez Ortiz
+# Copyright (c) 2022 Claudio Martínez Ortiz
 
 class Student
-
   attr_accessor :name
 
   def initialize(n, g)
-
     name = n
     @grade = g
-
   end
 
   def better_grade_than?(rival)
-
     nota > rival.nota
-
   end
 
   protected
 
   def nota
-    
     @grade
-
   end
-
 end
 
 perico = Student.new("Perico", 6)
 fulano = Student.new("Fulano", 5)
 puts perico.better_grade_than?(fulano)
-
